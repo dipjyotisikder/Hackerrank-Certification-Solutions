244a8c363ad8e6a1312b7d1f99d0212e03697072
dipjyoti <dip@mailinator.com>
2022-07-05 01:53:15 +0600

    Add project files.

---
+++ Hackerrank.Certifications.LongestSubArray/Program.cs
@@ -0,0 +1,78 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'longestSubarray' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int longestSubarray(List<int> arr)
+    {
+        if (arr.Count < 2)
+            return arr.Count;
+
+        int ultimate = 1;
+        int ultimateLow = 1;
+        int ultimateHigh = 1;
+
+        for (int i = 1; i < arr.Count; i++)
+        {
+            if (arr[i] == arr[i - 1])
+            {
+                ultimateLow++;
+                ultimateHigh++;
+
+            }
+            else if (arr[i] - 1 == arr[i - 1])
+            {
+                ultimateLow = 1 + ultimateHigh;
+                ultimateHigh = 1;
+
+            }
+            else if (arr[i] + 1 == arr[i - 1])
+            {
+                ultimateHigh = 1 + ultimateLow;
+                ultimateLow = 1;
+
+            }
+            else
+            {
+                ultimateLow = 1;
+                ultimateHigh = 1;
+            }
+
+            ultimate = Math.Max(Math.Max(ultimate, ultimateLow), Math.Max(ultimateLow, ultimateHigh));
+        }
+
+        return ultimate;
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = new List<int>();
+
+        for (int i = 0; i < arrCount; i++)
+        {
+            int arrItem = Convert.ToInt32(Console.ReadLine().Trim());
+            arr.Add(arrItem);
+        }
+
+        int result = Result.longestSubarray(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Hackerrank.Certifications.RoadRepair/Program.cs
@@ -0,0 +1,79 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'getMinCost' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY crew_id
+     *  2. INTEGER_ARRAY job_id
+     */
+
+    public static long getMinCost(List<int> crew_id, List<int> job_id)
+    {
+        crew_id.Sort();
+        job_id.Sort();
+
+        long totalDistance = 0;
+
+        for (int i = 0; i < crew_id.Count; i++)
+        {
+            long distance = Math.Abs(crew_id[i] - job_id[i]);
+            totalDistance += distance;
+        }
+
+        return totalDistance;
+
+    }
+
+}
+
+//for (int crewIdx = 0; crewIdx < crew_id.Count(); crewIdx++)
+//{
+//    for (int jobIdx = 0; jobIdx < job_id.Count(); jobIdx++)
+//    {
+//        if (crewIdx == jobIdx)
+//        {
+//            long distance = Math.Abs(crew_id[crewIdx] - job_id[jobIdx]);
+//            totalDistance += distance;
+
+//            break;
+//        }
+//    }
+//}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int crew_idCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> crew_id = new List<int>();
+
+        for (int i = 0; i < crew_idCount; i++)
+        {
+            int crew_idItem = Convert.ToInt32(Console.ReadLine().Trim());
+            crew_id.Add(crew_idItem);
+        }
+
+        int job_idCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> job_id = new List<int>();
+
+        for (int i = 0; i < job_idCount; i++)
+        {
+            int job_idItem = Convert.ToInt32(Console.ReadLine().Trim());
+            job_id.Add(job_idItem);
+        }
+
+        long result = Result.getMinCost(crew_id, job_id);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
